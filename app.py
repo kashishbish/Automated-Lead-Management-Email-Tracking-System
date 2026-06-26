3a2eb3d1d85d9bdd350844a53faf9fb350059f1b
Kashish Bisht <bishtkashish4608@gmail.com>
2026-06-26 16:47:40 +0530

    Fix PORT for Render deployment

--- app.py
+++ app.py
@@ -316,4 +316,6 @@ if __name__ == '__main__':
     print(" Lead Form: http://localhost:5000/")
     print(f"{'='*60}\n")
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
